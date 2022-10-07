52c5b18c4f72dc9799f448ed62693b6ba5bd23e0
rafaelEscola <105933809+rafaelEscola@users.noreply.github.com>
2022-10-07 12:33:54 -0300

    Update principal.js

--- principal.js
+++ principal.js
@@ -1,3 +1,9 @@
 document.querySelector(".titulo");)
 			var titulo = document.querySelector(".titulo");
-var paciente = document.query.Selector("")
+var paciente = document.query.Selector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+
+var peso = tdPeso.textContent;
+
+console.log(paciente);
+console.log(tdPeso);
