d2e767f4e9601ad1c99b809389f838d1ddc20d76
rafaelEscola <105933809+rafaelEscola@users.noreply.github.com>
2022-10-21 12:17:08 -0300

    Update principal.js

--- principal.js
+++ principal.js
@@ -11,6 +11,14 @@ var altura = tdAltura.textContente;
 var imc = peso / (altura * altura);
 
 var tdImc = paciente.querySelector(".info-imc");
+z
+if(peso < 0 > 1000){
+    console.log("Peso inválido);	
+}	
+
+if(altura < 0){
+    console.log("Peso inválido);	
+}		
 
 console.log(imc);
 
